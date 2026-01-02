12b808acfc6b0ae1917862c41abc0b86c5f51d42
MD. RAIHAN HOSSAIN <cit.mdraihanhossain@gmail.com>
2026-01-02 22:44:22 +0600

    Add decision making logic for rain status

---
+++ decision making.py
@@ -0,0 +1,12 @@
+# decision making
+# If there is rain --> stay at home
+# else --> go outside!
+
+# rain = bool(input("Give me the status of the rain:"))
+rain = False
+# conditional
+if(rain == True):
+  # indentation
+  print("Stay at Home!")
+else:
+  print("Go Outside!")
